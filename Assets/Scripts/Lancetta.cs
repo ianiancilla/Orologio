951be5fb2c0ff99a417f814d07a4c8226018986d
Marzia Faustinelli <marzia.faustinelli@gmail.com>
2021-01-25 21:15:13 +0100

    aggiunte lancette che seguono il mouse. posizione 0 sta alle ore 3

---
+++ Assets/Scripts/Lancetta.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lancetta : MonoBehaviour
+{
+    bool VengoTrascinata { get; set; }
+
+    void Start()
+    {
+        VengoTrascinata = false;
+    }
+
+    void Update()
+    {
+        // TODO check, se ci clicchi sopra lo attivi
+        VengoTrascinata = Input.GetMouseButton(0);
+
+        if (VengoTrascinata)
+        {
+            SeguiMouse();
+        }
+
+    }
+
+    private void SeguiMouse()
+    {
+        // determina vettore direzione mouse
+        Vector3 posizioneMouseRaw = Input.mousePosition;
+        Vector3 posizioneMouseScena = Camera.main.ScreenToWorldPoint(posizioneMouseRaw);
+
+        Vector2 direzioneMouse = new Vector2(posizioneMouseScena.x,
+                                             posizioneMouseScena.y).normalized;
+
+        // allinea oggetto alla direzione del mouse
+        float targetRotazioneZ = 1;
+        if (direzioneMouse.x > 0)
+        {
+            targetRotazioneZ = Mathf.Asin(direzioneMouse.y) * Mathf.Rad2Deg;
+        }
+        else
+        {
+            targetRotazioneZ = 180 - Mathf.Asin(direzioneMouse.y) * Mathf.Rad2Deg;
+        }
+
+        transform.localRotation = Quaternion.Euler(0, 0, targetRotazioneZ);
+    }
+}
---
+++ Assets/Scripts/Orologio.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Orologio : MonoBehaviour
+{
+    // costants
+    const string NOME_LANCETTA_ORE = "LancettaOre";
+    const string NOME_LANCETTA_MINUTI = "LancettaMinuti";
+    const string NOME_CENTRO_OROLOGIO = "CentroOrologio";
+
+
+    [SerializeField] Sprite spriteQuadrante;
+    [SerializeField] Sprite spriteLancettaOre;
+    [SerializeField] Sprite spriteLancettaMinuti;
+    [SerializeField] Sprite spriteCentroOrologio;
+    [SerializeField] Color coloreLancettaOre = Color.red;
+    [SerializeField] Color coloreLancettaMinuti = Color.green;
+
+    // member variables
+    Lancetta lancettaOre;
+    Lancetta lancettaMinuti;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // cache
+        lancettaOre = transform.Find(NOME_LANCETTA_ORE).GetComponent<Lancetta>();
+        lancettaMinuti = transform.Find(NOME_LANCETTA_MINUTI).GetComponent<Lancetta>();
+
+        SetupAspettoOrologio();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void SetupAspettoOrologio()
+    {
+        if (spriteLancettaOre)
+        {
+            SpriteRenderer renderer = transform.Find(NOME_LANCETTA_ORE).Find("Lancetta").GetComponent<SpriteRenderer>();
+            renderer.sprite = spriteLancettaOre;
+            renderer.color = coloreLancettaOre;
+        }
+
+        if (spriteLancettaOre)
+        {
+            SpriteRenderer renderer = transform.Find(NOME_LANCETTA_MINUTI).Find("Lancetta").GetComponent<SpriteRenderer>();
+            renderer.sprite = spriteLancettaMinuti;
+            renderer.color = coloreLancettaMinuti;
+        }
+
+        if (spriteCentroOrologio)
+        {
+            SpriteRenderer renderer = transform.Find(NOME_CENTRO_OROLOGIO).GetComponent<SpriteRenderer>();
+            renderer.sprite = spriteCentroOrologio;
+        }
+    }
+}
