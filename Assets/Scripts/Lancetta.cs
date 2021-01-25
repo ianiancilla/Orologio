1526f3ce20d383c4feffbe1ea762472ebbeabb2e
Marzia Faustinelli <marzia.faustinelli@gmail.com>
2021-01-25 22:21:23 +0100

    lancetta viene settata e sa ritornare il proprio angolo in gradi

--- Assets/Scripts/Lancetta.cs
+++ Assets/Scripts/Lancetta.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class Lancetta : MonoBehaviour
 {
-    bool VengoTrascinata { get; set; }
+    private bool VengoTrascinata { get; set; }
+    private float _angoloCorrente;
 
     void Start()
     {
+        SetAngoloLancetta(0);
         VengoTrascinata = false;
     }
 
@@ -20,7 +22,7 @@ public class Lancetta : MonoBehaviour
         {
             SeguiMouse();
         }
-
+        Debug.Log("angolo corrente " + _angoloCorrente);
     }
 
     private void SeguiMouse()
@@ -33,7 +35,7 @@ public class Lancetta : MonoBehaviour
                                              posizioneMouseScena.y).normalized;
 
         // allinea oggetto alla direzione del mouse
-        float targetRotazioneZ = 1;
+        float targetRotazioneZ;
         if (direzioneMouse.x > 0)
         {
             targetRotazioneZ = Mathf.Asin(direzioneMouse.y) * Mathf.Rad2Deg;
@@ -42,7 +44,33 @@ public class Lancetta : MonoBehaviour
         {
             targetRotazioneZ = 180 - Mathf.Asin(direzioneMouse.y) * Mathf.Rad2Deg;
         }
+        if (targetRotazioneZ <= 0)
+        {
+            targetRotazioneZ += 360;    // perché nel quadrante x/-y avevamo una rotazione da 0 a -90
+        }
 
-        transform.localRotation = Quaternion.Euler(0, 0, targetRotazioneZ);
+        // trova il valore dell'angolo in gradi
+        float nuovoAngolo = (360 - targetRotazioneZ) + 90;
+        if (nuovoAngolo > 360) { nuovoAngolo -= 360; }
+
+        SetAngoloLancetta(nuovoAngolo);
+    }
+
+    public void SetAngoloLancetta(float nuovoAngolo)
+    {
+        // check che sia inferiore a 360°
+        nuovoAngolo = nuovoAngolo % 360;
+        if (nuovoAngolo == 0) { nuovoAngolo = 360; }
+
+        _angoloCorrente = nuovoAngolo;
+
+        // calcola l'angolo di rotazione da applicare
+        float targetRotazioneZ;
+        targetRotazioneZ = 360 - nuovoAngolo;
+        targetRotazioneZ += 90;
+        targetRotazioneZ = targetRotazioneZ % 360;
+
+        // sposta la lancetta nella nuova posizione
+        transform.rotation = Quaternion.Euler(0, 0, targetRotazioneZ);
     }
 }
